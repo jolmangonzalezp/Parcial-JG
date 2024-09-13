6b1de9dab08e4d099882abd76d2f6056608db32f
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2024-09-12 19:22:27 -0500

    init

---
+++ parcialjolmangonzalez/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <router-link to="/">Emisoras</router-link> |
+    <router-link to="/about">Podcast</router-link>
+    <router-link to="/about">Mi Lista</router-link>
+    <router-link to="/about">Podcast</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
